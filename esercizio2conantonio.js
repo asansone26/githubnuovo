3224a1f02a381defb660e976227870bf4d84355d
asansone26 <asansone@studenti.apuliadigitalmaker.it>
2022-03-02 14:05:28 +0100

    ciao

--- esercizio2conantonio.js
+++ esercizio2conantonio.js
@@ -1,4 +1,10 @@
-var numeriCasuali= Math.random(); //numerocasuale
+
+var listaNumeri = new Array();
 for (i=0; i<10; i++){
+   listaNumeri[i] = Math.random();
+   
+
     
 }
+var Math = Math.max (...listaNumeri);
+var Minore= Math.min(... listaNumeri);
